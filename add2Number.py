82b00d2b0261e3d5569bc6a6ec93dc84c5c63b6c
S V Akshay Kumar <adevadiga2005@gmail.com>
2025-03-04 12:04:44 +0530

    Add 2 numbers

---
+++ add2Number.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+
+
+sum_Result = num1 + num2
+
+print("The sum of {num1} and {num2} is equal to ",sum_Result)
