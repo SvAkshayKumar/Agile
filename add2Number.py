74c613ca4ab197411bb5b07068a967242f9b2b3a
S V Akshay Kumar <adevadiga2005@gmail.com>
2025-03-11 12:42:16 +0530

    Lab7 Docker

--- add2Number.py
+++
@@ -1,8 +0,0 @@
-num1 = float(input("Enter first number : "))
-num2 = float(input("Enter second number : "))
-
-
-sum_Result = num1 + num2
-
-print("The sum of {num1} and {num2} is equal to ",sum_Result)
-print("{num1} + {num2} = {sum_Result}")
